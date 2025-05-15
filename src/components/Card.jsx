876adf4dbabe390bd7477f290e0097deaccdc9b6
Danny Burrow <burrowingdan@gmail.com>
2025-05-15 16:18:24 -0700

    enable voting on cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react"
+
 function Card({ card, handleDelete }) {
-  // TODO: fill in
+  const [newCard, setNewCard] = useState(card)
+
+  const vote = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/cards/${card.id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ voteCount: newCard.voteCount + 1 }),
+        }
+      )
+      if (!response.ok) throw new Error("Network response was not ok")
+      const data = await response.json()
+      setNewCard(data)
+    } catch (error) {
+      console.error("Error voting:", error)
+    }
+  }
+
   return (
     <div key={card.id} className="card">
       <h2>{card.text}</h2>
       <img src={card.gifUrl} alt="" />
-      <button onClick={() => handleDelete(card.id)}>Delete</button>
+      <div className="button-wrapper">
+        <button onClick={vote} className="coral">
+          Votes: {newCard.voteCount}
+        </button>
+        <button onClick={() => handleDelete(card.id)}>Delete</button>
+      </div>
     </div>
   )
 }
